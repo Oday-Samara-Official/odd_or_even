fb708af4c3eaa8099ae7167be2b9c7fcc9b85d90
oday-samara-Official <113715194+oday-samara-Official@users.noreply.github.com>
2022-09-16 13:30:59 -0400

    Add files via upload

---
+++ odd_or_even.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("Enter the number");
+    scanf("%d",&num);
+    (num % 2 == 0)?
+    printf("even"):
+    printf("odd");
+
+}
